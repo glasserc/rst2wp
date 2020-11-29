f5550fa71430303c41a0715aa4924ac34cbc070b
Ethan Glasser-Camp <ethan@betacantrips.com>
2020-11-28 22:11:52 -0500

    Fix test_upload through judicious use of mocking

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -10,19 +10,33 @@ class TestUpload(unittest.TestCase):
         self.state_machine = mock.Mock()
         self.up = upload.UploadDirective('upload', [filename], {}, None, 'lineno', 'content_offset', '.. upload::', 'state', self.state_machine)
 
+    def mock_run(self):
+        def fake_os_stat(filename):
+            m = mock.Mock()
+            m.st_size = 1337
+            return m
+
+        with mock.patch('os.path.exists') as os_path_exists:
+            with mock.patch('os.stat') as os_stat:
+                with mock.patch('magic.open') as magic_open:
+                    os_path_exists.side_effect = lambda filename: filename.endswith('file.odf')
+                    os_stat.side_effect = fake_os_stat
+                    magic_open().file.side_effect = 'text/plain'
+                    return self.up.run()
+
     def test_upload(self):
         self.create_directive('/path/to/file.odf')
         self.assertEqual(self.up.arguments, ['/path/to/file.odf'])
 
-        output = self.up.run()
+        output = self.mock_run()
 
         assert self.state_machine.document.settings.wordpress_instance.upload_file.called
         self.assertEqual(self.state_machine.document.settings.wordpress_instance.upload_file.call_args_list,
-                         [(('/path/to/file.odf',), {})])
+                         [mock.call('/tmp/file.odf')])
 
     def test_no_upload(self):
         self.create_directive('/path/to/file.odf')
         self.up.options['uploaded'] = 'http://example.com/already/exists'
 
-        output = self.up.run()
+        output = self.mock_run()
         self.assertFalse(self.state_machine.document.settings.wordpress_instance.upload_file.called)
